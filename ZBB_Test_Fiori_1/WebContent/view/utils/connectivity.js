153949a3436f9f86b2ce4f6d2dbdf6cd31577bd9
bharatbjj <bharatbjj@yahoo.co.in>
2019-01-01 13:53:55 +0530

    V1

---
+++ ZBB_Test_Fiori_1/WebContent/view/utils/connectivity.js
@@ -0,0 +1,10 @@
+var hostPort = "";
+
+// this setting is required to test app with odataService in Eclipse environment
+//hostPort	 = "proxy/http/<host>:<port>";
+
+//Odata Service Path
+var oDataPath = "/sap/opu/odata/sap/ZTEST_ODATA_SRV/";
+
+// Odata Service Url
+var serviceUrl= hostPort + oDataPath;
---
+++ ZBB_Test_Fiori_1/WebContent/zbb_test_fiori_1/Master.controller.js
@@ -0,0 +1,38 @@
+sap.ui.controller("zbb_test_fiori_1.Master", {
+
+/**
+* Called when a controller is instantiated and its View controls (if available) are already created.
+* Can be used to modify the View before it is displayed, to bind event handlers and do other one-time initialization.
+* @memberOf zbb_test_fiori_1.Master
+*/
+//	onInit: function() {
+//
+//	},
+
+/**
+* Similar to onAfterRendering, but this hook is invoked before the controller's View is re-rendered
+* (NOT before the first rendering! onInit() is used for that one!).
+* @memberOf zbb_test_fiori_1.Master
+*/
+//	onBeforeRendering: function() {
+//
+//	},
+
+/**
+* Called when the View has been rendered (so its HTML is part of the document). Post-rendering manipulations of the HTML could be done here.
+* This hook is the same one that SAPUI5 controls get after being rendered.
+* @memberOf zbb_test_fiori_1.Master
+*/
+//	onAfterRendering: function() {
+//
+//	},
+
+/**
+* Called when the Controller is destroyed. Use this one to free resources and finalize activities.
+* @memberOf zbb_test_fiori_1.Master
+*/
+//	onExit: function() {
+//
+//	}
+
+});
